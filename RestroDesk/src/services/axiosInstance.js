9d2f212c43c7a1fd3837a674b3fbbeecb9725e2a
Prince gond <princegondrw123@gmail.com>
2026-06-09 15:05:21 +0530

    ...

--- RestroDesk/src/services/axiosInstance.js
+++ RestroDesk/src/services/axiosInstance.js
@@ -2,3 +2,10 @@ import axios from 'axios';
 const baseURL = 'http://localhost:3001';   // ← yeh line check karo
 const axiosInstance = axios.create({ baseURL });
 export default axiosInstance;
+
+
+
+
+
+
+
